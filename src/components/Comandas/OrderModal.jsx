36b4783abca647e061b16e1790f598a0604274f4
Pablo Fuentes <pablo.fuentes@macq.eu>
2024-01-19 19:50:19 +0100

    fixup! rebranding

--- src/components/Comandas/OrderModal.jsx
+++ src/components/Comandas/OrderModal.jsx
@@ -1,22 +1,39 @@
 import {
   Modal,
-  Input,
   Button,
-  Textarea,
   ModalFooter,
   ModalBody,
   ModalHeader,
   ModalContent,
 } from '@nextui-org/react';
+import { mockedCategories } from '../../mocks/mockedCategories';
+import { useState } from 'react';
 
 function OrderModal({ isOpen, onClose, table }) {
+  const [categories, setCategories] = useState(mockedCategories);
   return (
     <Modal isOpen={isOpen} onClose={() => {}} hideCloseButton size="5xl">
       <ModalContent>
         <ModalHeader>
           <h3>Comanda: {table?.nombreMesa}</h3>
         </ModalHeader>
-        <ModalBody></ModalBody>
+        <ModalBody>
+          <div className="flex w-full">
+            <div className="w-1/3">hola</div>
+            <div className="w-2/3 flex gap-5 flex-wrap">
+              {Object.keys(categories).map((category) => {
+                //Hacer Componente
+                return (
+                
+                  <div className="w-1/6 text-center cursor-pointer" key={category}>
+                    <img src={categories[category].img} alt="" className='rounded'/>
+                    <h3 className='text-xl'>{category}</h3>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </ModalBody>
         <ModalFooter>
           <Button auto flat color="error" onClick={onClose}>
             Cerrar
--- src/mocks/mockedCategories.jsx
+++ src/mocks/mockedCategories.jsx
@@ -1,81 +1,85 @@
+import ensalada from '../assets/img/categories/ensaladas.png'
+import comidas from '../assets/img/categories/comidas.png'
+import horneados from '../assets/img/categories/horneados.png'
+
 export const mockedCategories = {
     "Aceites": {
       id: "cat-001",
       items: ['ing-001'],
       description: "Diversos tipos de aceites, incluyendo aceite de oliva, vegetal, etc.",
-      img: ""
+      img: ensalada
     },
     "Básicos": {
       id: "cat-002",
       items: ['ing-001', 'ing-002', 'ing-022'],
       description: "Ingredientes básicos y esenciales para cualquier cocina.",
-      img: ""
+      img: ensalada
     },
     "Carnes": {
       id: "cat-003",
       items: ['ing-008'],
       description: "Variedad de carnes, incluyendo pollo, res, cerdo, etc.",
-      img: ""
+      img: ensalada
     },
     "Condimentos": {
       id: "cat-004",
       items: ['ing-009', 'ing-010'],
       description: "Condimentos y especias para realzar el sabor de los platos.",
-      img: ""
+      img: ensalada
     },
     "Frescos": {
       id: "cat-005",
       items: ['ing-025'],
       description: "Productos frescos como frutas y verduras.",
-      img: ""
+      img: ensalada
     },
     "Harinas": {
       id: "cat-006",
       items: ['ing-002'],
       description: "Diferentes tipos de harinas, incluyendo harina de trigo, maíz, etc.",
-      img: ""
+      img: ensalada
     },
     "Lácteos": {
       id: "cat-007",
       items: ['ing-004', 'ing-024', 'ing-023'],
       description: "Productos lácteos como leche, queso, yogur, etc.",
-      img: ""
+      img: ensalada
     },
     "Quesos": {
       id: "cat-008",
       items: ['ing-024', 'ing-023'],
       description: "Variedad de quesos, desde mozzarella hasta parmesano.",
-      img: ""
+      img: ensalada
     },
     "Vegetales": {
       id: "cat-009",
       items: ['ing-003', 'ing-006'],
       description: "Diversos vegetales, incluyendo tomates, cebollas, lechugas, etc.",
-      img: ""
+      img: ensalada
     },
     "Verduras": {
       id: "cat-010",
       items: ['ing-025'],
       description: "Amplia gama de verduras frescas y nutritivas.",
-      img: ""
+      img: ensalada
     },
     "Horneados": {
       id: "cat-011",
       items: ['ing-022'],
       description: "Productos horneados como panes, pasteles, etc.",
-      img: ""
+      img: horneados
     },
     "Comidas": {
       id: "cat-012",
       items: ["prod-001"],
       description: "Platillos preparados y listos para servir.",
-      img: ""
+      img: comidas
     },
     "Ensaladas": {
       id: "cat-013",
       items: ["prod-002"],
       description: "Diversas ensaladas, desde clásicas hasta innovadoras.",
-      img: ""
+      img: ensalada
     }
   };
   
